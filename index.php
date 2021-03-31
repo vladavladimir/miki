6ff349d01f062b1ed3709425ece9eda36e58ce26
vlada <vladimir1987.markovic@gmail.com>
2021-03-31 15:23:56 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<div id="wrep">
+<div><p>afghjk</p></div>
+<div><p>dsfghj</p></div>
+</div
+</body>
+<style type="text/css">
+	#wrap{
+		display: inline;
+		width: 300px;
+		height: 300px;
+		margin-left: 30px;
+	}
+</style>
+</html>
